5054dfdaedee9b98ca8c8ebf1d4d8fd5e3b3927e
Maciej Caputa <maciej.caputa.pl@gmail.com>
2016-11-20 17:22:23 +0000

    Firebase progress.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -30,74 +30,271 @@
     /** Initialisation of Angular's module, ngResource is injected since it is needed to have access to $resource object.*/
     .module('app', ['ngResource', 'firebase'])
 
-    /** Initialisation of Angular's controller, $scope is passed as default, $resource is used to query Google's finance API to get real time data, $http is used to query Quandl EOD to get historic data.*/
-    .controller('ctrl', ['$scope', '$firebaseObject', '$firebaseAuth', '$resource', '$http', function($scope, $firebaseObject, $firebaseAuth, $resource, $http) {
+    .factory('apiRealTimeQuotes', ['$resource', function apiRealTimeQuotesFactory($resource) {
 
+
+      // Helper function to create combined queries to google's api.
+      function constructQuery( market ) {
+        let query = '', i = 0, key;
+        for (key in window[ market ]) {
+          if (i > 0) {
+            query += ',';
+          }
+          i++;
+          query += market + ':' + key;
+        }
+        return query;
+      };
+
+      /**
+       * Getting quotes for specific market.
+       * @param {string} marketSymbol - symbol of a stock market
+       * @returns {Array} Returns an array of objects representing quotes statisitcs of a company.
+       */
+      function getMarketQuotes( marketSymbol ) {
+        let
+          query = constructQuery( marketSymbol.toUpperCase() ),
+          googleFinance = $resource( config.googleBaseUrl,
+                                      { client:'ig', callback:'JSON_CALLBACK'},
+                                      { get: { method:'JSONP', params:{ q: query }, isArray: true } });
+
+        // console.info( '|-----' );
+        // console.info( googleFinance.prototype.$get() );
+        // console.info( googleFinance.prototype );
+        // console.info( googleFinance.prototype.constructor.name );
+        // console.info( googleFinance.get('A') );
+        // console.info( googleFinance );
+        // console.info( '-----' );
+
+        return googleFinance.get(0);
+      }
+
+      /**
+       * Request to Google's finance API to get FTSE 100 and NASDAQ 100 stock quotes.
+       */
       let
-        rootRef = firebase.database().ref(),
-        ref = rootRef.child('currentUser'),
-        syncObject = $firebaseObject(ref);
+        quotesFTSE = getMarketQuotes( 'FTSE' ),
+        quotesNASDAQ = getMarketQuotes( 'NASDAQ' ),
+        quotes = [].concat(quotesFTSE, quotesNASDAQ);
 
-      $scope.currentUser = syncObject;
-      syncObject.$bindTo($scope, "currentUser");
+      //
+      // console.info( quotesFTSE );
+      // console.log( quotesFTSE );
+      // console.log(quotesNASDAQ);
+      // console.log(quotes);
+      return quotesFTSE;
+    }])
 
-      $scope.currentUser = $firebaseObject(ref);
+    .factory('apiHistoricQuotes', ['clientId', function apiHistoricQuotesFactory(companyCode) {
+
+      /**
+       * Getting historic quotes for a specific company.
+       * @param {string} company - symbol of a company
+       * @returns {Array} array of two-element arrays of date and end of the day quote.
+       */
+      $scope.getCompany = function(company) {
+        let collapse, market, url;
+
+        // When choosing a company card its symbol is saved and time span is set to default to one year.
+        if (company !== undefined) {
+          $scope.currentCompany = company;
+          $scope.timeSpan = 366;
+        }
+
+         // Choosing an appropriate collapse for uplled data.
+        if ($scope.timeSpan > 366) {
+          collapse = 'weekly';
+          $scope.timeSpan = Math.floor( $scope.timeSpan / 7 );
+        } else {
+          collapse = 'daily';
+        }
+
+        // Getting a corresponding symbol of a market.
+        market = NASDAQ[$scope.currentCompany] ? 'NASDAQ' : 'LON';
+
+        // Creating a request url.
+         url = `${config.quandlBaseUrl}${market}_${$scope.currentCompany}.json`;
+
+
+        // Making request to Quandl's API to get end of the day historic data.
+        $http({method: 'GET', url: url, params: {api_key: config.quandlApiKey, rows: $scope.timeSpan, column_index: 4, collapse: collapse }})
+          .then( (response) => {
+            plotGraph( response.data.dataset );
+            return response.data.dataset;
+          })
+          .then(() => {
+            plotGraph( $scope.company.data );
+          })
+          .catch((err) => {
+            // In case of error print error response.
+            console.error(err);
+            return null;
+          });
+        };
+    }])
+
+    /** Initialisation of Angular's controller, $scope is passed as default, $resource is used to query Google's finance API to get real time data, $http is used to query Quandl EOD to get historic data.*/
+    .controller('ctrl', ['$scope', '$firebaseObject', '$firebaseAuth', '$resource', '$http', 'apiRealTimeQuotes', function($scope, $firebaseObject, $firebaseAuth, $resource, $http, apiRealTimeQuotes) {
+
+      // console.log(apiRealTimeQuotes);
+      // console.log(apiRealTimeQuotes);
+      // console.log(apiRealTimeQuotes);
+
+      // Initialisation
+      $scope.login = {};
+      $scope.signup = {};
+      $scope.quotes = [];
+      $scope.user = firebase.auth().currentUser;
 
       $scope.signUp = function() {
         firebase
           .auth()
-          .createUserWithEmailAndPassword($scope.email, $scope.password)
-          .catch(function(error) {
-
-            let errorCode = error.code;
-
-            $scope.errorMessage = error.message;
-
+          .createUserWithEmailAndPassword($scope.signup.email, $scope.signup.password)
+          .then(() => {
+            firebase
+              .auth()
+              .currentUser
+              .updateProfile({
+                displayName: `${$scope.signup.firstName} ${$scope.signup.lastName}`,
+                photoURL: 'https://maciejcaputa.com/assets/images/thumbnail-face.jpg'
+              })
+              .then(() => {
+                firebase
+                  .database()
+                  .ref(`users/${firebase.auth().currentUser.uid}`)
+                  .set({
+                    firstName: $scope.signup.firstName,
+                    lastName: $scope.signup.lastName,
+                    name: `${$scope.signup.firstName} ${$scope.signup.lastName}`,
+                    email: $scope.signup.email,
+                    avatar: 'https://maciejcaputa.com/assets/images/thumbnail-face.jpg',
+                    balance: 10000,
+                    transactions: {
+                      open: [
+                        {
+                          symbol: "AAPL",
+                          name: "Apple Inc.",
+                          price: {
+                            open: 113.05
+                          },
+                          shares: 90,
+                          date: ""
+                        },
+                        {
+                          symbol: "GOOGL",
+                          name: "Alphabet Inc Class A",
+                          price: {
+                            open: 801.23
+                          },
+                          shares: 12,
+                          date: ""
+                        },
+                        {
+                          symbol: "MSFT",
+                          name: "Microsoft Corp",
+                          price: {
+                            open: 57.96
+                          },
+                          shares: 175,
+                          date: ""
+                        }
+                      ],
+                      closed: [
+                        {
+                          symbol: "AMZN",
+                          name: "Amazon.com Inc.",
+                          price: {
+                            open: 737.61,
+                            close: 844.36
+                          },
+                          shares: 10,
+                          date: "",
+                        },
+                        {
+                          symbol: "NFLX",
+                          name: "Netflix Inc.",
+                          price: {
+                            open: 82.79,
+                            close: 106.28
+                          },
+                          shares: 100,
+                          date: "",
+                        },
+                      ]
+                    }
+                });
+              })
+              .then(() => {
+                firebase
+                  .database()
+                  .ref(`/users/${firebase.auth().currentUser.uid}`)
+                  .once('value')
+                  .then((snapshot) => {
+                    $scope.user = snapshot.val();
+                    $scope.view = 'quotes';
+                    $scope.$apply();
+                    console.info('--- LOG IN SUCCEEDED ---');
+                    console.info($scope.user);
+                  });
+              });
+          })
+          .catch((error) => {
+            $scope.signup.errorMessage = error.message;
+            $scope.$apply();
+            console.error('--- SIGN UP FAILED ---');
             console.error(error.code);
             console.error(error.message);
-
         });
       };
 
       $scope.logIn = function() {
         firebase
           .auth()
-          .signInWithEmailAndPassword($scope.email, $scope.password)
-          .then(function() {
-            $scope.user = firebase.auth().currentUser.providerData[0];
-            $scope.$apply();
-            console.log($scope.user);
-
-            console.log("Sign-in provider: "+$scope.user.providerId);
-            console.log("  Provider-specific UID: "+$scope.user.uid);
-            console.log("  Name: "+$scope.user.displayName);
-            console.log("  Email: "+$scope.user.email);
-            console.log("  Photo URL: "+$scope.user.photoURL);
-
+          .signInWithEmailAndPassword($scope.login.email, $scope.login.password)
+          .then(() => {
+            firebase
+              .database()
+              .ref(`/users/${firebase.auth().currentUser.uid}`)
+              .once('value')
+              .then((snapshot) => {
+                $scope.user = snapshot.val();
+                $scope.$apply();
+                console.info('--- LOG IN SUCCEEDED ---');
+                console.info($scope.user);
+              });
           })
-          .catch(function(error) {
-
-            let errorCode = error.code;
-
-            $scope.errorMessage = error.message;
+          .catch((error) => {
+            $scope.login.errorMessage = error.message;
             $scope.$apply();
-
+            console.error('--- LOG IN FAILED ---');
             console.error(error.code);
             console.error(error.message);
-
         });
       };
 
+      /**
+       * Evaluates student's balance.
+       * Adds the transaction to closed transactions.
+       * Removes the transaction from open transactions.
+       * Updates changes in the database.
+       */
+      $scope.buy = function(quote, shares) {
+        // TODO: IF logged in
+        // TODO: If has enough money
 
-
-      // Uncomment only for testing purposes
-      // syncObject.$loaded()
-      //   .then(function(e) {
-      //     $scope.currentUser = CURRENT_USER;
-      //   })
-      //  .catch(function(error) {
-      //     console.error("Error:", error);
-      //   });
+        firebase
+          .database()
+          .ref(`users/${firebase.auth().currentUser.uid}/transactions/open`)
+          .push({
+            symbol: quote.symbol,
+            name: quote.name,
+            price: {
+              open: quote.level
+            },
+            shares: shares,
+            date: new Date().now()
+        });
+      };
 
       /**
        * Evaluates student's balance.
@@ -106,6 +303,8 @@
        * Updates changes in the database.
        */
       $scope.sell = function(index) {
+        // TODO: If logged in
+        // TODO: IF has enough shares
         $scope.currentUser.balance = parseInt($scope.currentUser.balance, 10 )
                                    + parseInt( $scope.currentUser.transactions.open[index].marketValue, 10 );
         $scope.currentUser.transactions.closed.push( $scope.currentUser.transactions.open[index] );
@@ -118,7 +317,7 @@
       $scope.FTSE = FTSE;
 
       // Initialisation
-      $scope.currentUser = CURRENT_USER;
+      // $scope.currentUser = CURRENT_USER;
       $scope.view = 'quotes';
 
       /**
@@ -127,24 +326,76 @@
        * @returns {Array} Returns an array of objects representing quotes statisitcs of a company.
        */
       function getMarketQuotes( marketSymbol ) {
+
         let
-        query = constructQuery( marketSymbol.toUpperCase() ),
-            googleFinance = $resource( config.googleBaseUrl,
-                                      { client:'ig', callback:'JSON_CALLBACK'},
-                                      { get: { method:'JSONP', params:{ q: query }, isArray: true } });
-        return googleFinance.get();
+          query = constructQuery( marketSymbol.toUpperCase() ),
+          quotes = '';
+
+
+        return $http.jsonp(`https://finance.google.com/finance/info?client=ig&q=${query}&callback=JSON_CALLBACK`)
+          .then(function(response) {
+            let quotes = response.data;
+
+            for (let key in quotes) {
+              let quote = quotes[key];
+              // console.log(quote);
+              quotes[key] = {
+                symbol: quote.t,
+                name: NASDAQ.hasOwnProperty(quote.t) ? NASDAQ[ quote.t ] : FTSE[ quote.t ],
+                level: parseFloat( quote.e == 'LON' ? (quote.l.replace(',','') / 100) : quote.l ),
+                change: parseFloat( quote.c, 10),
+                changePercent: parseFloat( quote.cp ),
+                timestamp: new Date(quote.lt_dts),
+                market: quote.e == 'LON' ? 'FTSE 100' : 'NASDAQ 100',
+              };
+              // console.log(quote);
+            }
+
+            console.log(quotes);
+            return quotes;
+          })
+          .catch(function(error) {
+            console.error( error );
+          });
+
+
       }
 
       /**
        * Request to Google's finance API to get FTSE 100 and NASDAQ 100 stock quotes.
        */
       function getQuotes() {
-        $scope.quotesFTSE = getMarketQuotes( 'FTSE' );
-        $scope.quotesNASDAQ = getMarketQuotes( 'NASDAQ' );
+        let quotesFTSE, quotesNASDAQ;
+
+        getMarketQuotes( 'FTSE' )
+          .then((quotes) => {
+            quotesFTSE = quotes;
+            console.info('quotes');
+            console.info(quotes);
+            console.info('quotes');
+          });
+
+          getMarketQuotes( 'NASDAQ' )
+            .then((quotes) => {
+              quotesNASDAQ = quotes;
+              console.info('quotes');
+              console.info(quotes);
+              console.info('quotes');
+            }).
+            then(() => {
+              $scope.quotes = [].concat(quotesFTSE, quotesNASDAQ);
+            });
+
+
+
       }
+
+
 
       // Initial request on page load.
       getQuotes();
+
+
 
       // Stock quotes are updated every minute.
       setInterval(function() {
@@ -236,7 +487,7 @@
 
     }]);
 
-    // Helper function to create combined queries to google's api.
+  // Helper function to create combined queries to google's api.
   function constructQuery( market ) {
     let query = '', i = 0, key;
     for (key in window[ market ]) {
